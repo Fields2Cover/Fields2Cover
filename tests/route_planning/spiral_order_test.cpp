9f4107f14763bf1be5acff6d6ca6a957461cd5ce
Tomáš Lokša <tomas.loksa@outlook.com>
2022-07-28 07:42:14 +0200

    add tests for spiral_order

---
+++ tests/route_planning/spiral_order_test.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include "fields2cover/types.h"
+#include "fields2cover/route_planning/spiral_order.h"
+
+TEST(fields2cover_route_spiral, genSortedSwaths_even) {
+  const int n = 11;
+  const int size = 6;
+  F2CSwaths swaths;
+  for (int i = 1; i < n; ++i) {
+    swaths.emplace_back(F2CLineString({F2CPoint(0, i), F2CPoint(1, i)}), i, i);
+  }
+
+  f2c::rp::SpiralOrder swath_sorter(swaths, size);
+
+  swaths = swath_sorter.genSortedSwaths();
+
+  EXPECT_EQ(swaths[0].getWidth(), 1);
+  EXPECT_EQ(swaths[1].getWidth(), 6);
+  EXPECT_EQ(swaths[2].getWidth(), 2);
+  EXPECT_EQ(swaths[3].getWidth(), 5);
+  EXPECT_EQ(swaths[4].getWidth(), 3);
+  EXPECT_EQ(swaths[5].getWidth(), 4);
+  EXPECT_EQ(swaths[6].getWidth(), 7);
+  EXPECT_EQ(swaths[7].getWidth(), 10);
+  EXPECT_EQ(swaths[8].getWidth(), 8);
+  EXPECT_EQ(swaths[9].getWidth(), 9);
+}
+
+TEST(fields2cover_route_spiral, genSortedSwaths_odd) {
+  const int n = 11;
+  const int size = 5;
+  F2CSwaths swaths;
+  for (int i = 1; i < n; ++i) {
+    swaths.emplace_back(F2CLineString({F2CPoint(0, i), F2CPoint(1, i)}), i, i);
+  }
+
+  f2c::rp::SpiralOrder swath_sorter(swaths, size);
+
+  swaths = swath_sorter.genSortedSwaths();
+
+  EXPECT_EQ(swaths[0].getWidth(), 1);
+  EXPECT_EQ(swaths[1].getWidth(), 5);
+  EXPECT_EQ(swaths[2].getWidth(), 2);
+  EXPECT_EQ(swaths[3].getWidth(), 4);
+  EXPECT_EQ(swaths[4].getWidth(), 3);
+  EXPECT_EQ(swaths[5].getWidth(), 10);
+  EXPECT_EQ(swaths[6].getWidth(), 6);
+  EXPECT_EQ(swaths[7].getWidth(), 9);
+  EXPECT_EQ(swaths[8].getWidth(), 7);
+  EXPECT_EQ(swaths[9].getWidth(), 8);
+}
