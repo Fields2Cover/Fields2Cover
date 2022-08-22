dfdd0d3c90284e25a94d78eecdf54eb78aa0e915
sanson-robotics <gonzalo.miermunoz@wur.nl>
2022-08-23 01:05:21 +0200

    Fix: error concatenating str + int

--- tests/python/path_planning/PP_checker.py
+++ tests/python/path_planning/PP_checker.py
@@ -9,8 +9,8 @@ import fields2cover as f2c
 
 
 def IsPathCorrect(path, start, start_ang, end, end_ang, check_y_lower_limit = True):
-  assert not (path.points.size() < 3),                            \
-      "Error 3001: The path has only " + path.points.size() +     \
+  assert not (path.points.size() < 3),                              \
+      "Error 3001: The path has only " + str(path.points.size()) +  \
       "points. Expected at least 3 points.";
   
   assert not (check_y_lower_limit and
@@ -18,22 +18,22 @@ def IsPathCorrect(path, start, start_ang, end, end_ang, check_y_lower_limit = Tr
        "Error 3002: Lower limit on Y axis is crossed.";
   assert not ((abs(path.points[0].getX() - start.getX()) > 1e-1) or
     (abs(path.points[0].getX() - start.getX()) > 1e-1)),            \
-      "Error 3003: Start point should be " + start + ", but is " +  \
+      "Error 3003: Start point should be " + str(start) + ", but is " +  \
       path.points[0] + ".";
   assert not ((abs(path.points[0].getX() - start.getX()) > 1e-1) or
     (abs(path.points[0].getX() - start.getX()) > 1e-1)),            \
-      "Error 3004: End point should be " + end + ", but is " +      \
-      path.points.back() +".";
+      "Error 3004: End point should be " + str(end) + ", but is " +      \
+      str(path.points.back()) +".";
   assert (path.isValid()), "Error 3005: Path is not valid.";
   assert not (f2c.Point.getAngleDiffAbs(path.angles[0], start_ang) > 0.1),  \
-      "Error 3006: Start angle should be " + start_ang +                    \
-      ", but computed angle is " + path.angles[0] + ".";
+      "Error 3006: Start angle should be " + str(start_ang) +                    \
+      ", but computed angle is " + str(path.angles[0]) + ".";
   assert not (f2c.Point.getAngleDiffAbs(path.angles.back(), end_ang) > 0.1), \
-      "Error 3007: End angle should be " + end_ang +                         \
-      ", but computed angle is " + path.angles.back() + ".";
+      "Error 3007: End angle should be " + str(end_ang) +                         \
+      ", but computed angle is " + str(path.angles.back()) + ".";
   assert not (start.Distance(end) > path.length()),                        \
-      "Error 3008: Length of the curve (" + path.length() +                \
+      "Error 3008: Length of the curve (" + str(path.length()) +                \
       ") is smaller than the distance between start to end points (" +     \
-      start.Distance(end) + ").";
+      str(start.Distance(end)) + ").";
   return True
 
--- tests/python/swath_generator/SG_checker.py
+++ tests/python/swath_generator/SG_checker.py
@@ -15,27 +15,26 @@ def IsSwathGenerationCorrect(sw_gen, poly, swath_width = 1.0, checks = 100):
   swaths = sw_gen.generateBestSwaths(swath_width, poly);
   assert not (sw_gen.getBestAngle() == -1e7), "Error 2000: Best angle not set.";
   assert not (swaths.size() < 1), "Error 2001: No swaths were generated.";
-  assert not (swaths.size() > poly.getArea() / (swath_width * swath_width)),  \
-      "Error 2002: " + swaths.size() + " swaths were generated." +            \
-      "Expected less than " + poly.getArea() / (swath_width * swath_width) +  \
+  assert not (swaths.size() > poly.getArea() / (swath_width * swath_width)),       \
+      "Error 2002: " + str(swaths.size()) + " swaths were generated." +            \
+      "Expected less than " + str(poly.getArea() / (swath_width * swath_width)) +  \
       "swaths.";
 
   rand = f2c.Random();
   for i in range(checks):
     rand_ang = rand.getAngleRandom();
     swaths2 = sw_gen.generateSwaths(rand_ang, swath_width, poly);
-    sign = 0.0;
-    assert not (sw_gen.computeCostWithMinimizingSign(swaths) >                  \
+    assert (sw_gen.computeCostWithMinimizingSign(swaths) <=                    \
             sw_gen.computeCostWithMinimizingSign(swaths2)),                     \
             "Error 2003: Cost (signed) of the optimal angle (" +                \
-            sw_gen.getBestAngle() + ") is " +                                   \
-            sign * sw_gen.computeCost(swaths) +                                 \
+            str(sw_gen.getBestAngle()) + ") is " +                              \
+            str(sw_gen.computeCostWithMinimizingSign(swaths)) +                            \
             ", but the cost (signed) of a non-optimal angle (" +                \
-            rand_ang + ") is " +                                                \
-            sw_gen.computeCostWithMinimizingSign(swaths2) + ".";
+            str(rand_ang) + ") is " +                                                \
+            str(sw_gen.computeCostWithMinimizingSign(swaths2)) + ".";
     assert not (swaths.size() > poly.getArea() / (swath_width * swath_width)), \
-        "Error 2004: " + swaths2.size() +                                      \
-        " swaths were generated with angle " + rand_ang +                      \
-        "Expected less than " + poly.getArea() / (swath_width * swath_width) + \
+        "Error 2004: " + str(swaths2.size()) +                                      \
+        " swaths were generated with angle " + str(rand_ang) +                      \
+        "Expected less than " + str(poly.getArea() / (swath_width * swath_width)) + \
         "swaths.";
   
