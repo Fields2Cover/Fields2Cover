c05a4fb243d38728e2cf581c3733d1584e0edf3c
Gonzalo Mier <gonzalo.miermunoz@wur.nl>
2022-10-10 09:50:56 +0200

    Removed warnings building dos and added favicon

--- docs/conf.py
+++ docs/conf.py
@@ -83,9 +83,10 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 html_logo = "figures/logo_fields2cover.png"
+html_favicon = "figures/favicon/favicon.ico"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
