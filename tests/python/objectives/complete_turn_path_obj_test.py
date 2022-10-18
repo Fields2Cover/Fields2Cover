bbad53984c7ed805058ae9cdf4d39c2e1b0180d7
Gonzalo Mier <gonzalo.miermunoz@wur.nl>
2022-10-18 13:12:08 +0200

    Change a python test

--- tests/python/objectives/complete_turn_path_obj_test.py
+++ tests/python/objectives/complete_turn_path_obj_test.py
@@ -16,7 +16,7 @@ def test_fields2cover_obj_complete_turn_path_obj_computeCost():
   swath1 = f2c.Swath(f2c.LineString(f2c.VectorPoint(
       [f2c.Point(0.0, 0.0), f2c.Point(0.0, 1.0)])));
   swath2 = f2c.Swath(f2c.LineString(f2c.VectorPoint(
-      [f2c.Point(3.0, 1.0), f2c.Point(3.0, 0.0)])));
+      [f2c.Point(1.0, 1.0), f2c.Point(1.0, 0.0)])));
   swaths1 = f2c.Swaths();
   [swaths1.push_back(i) for i in [swath1]]
   swaths2 = f2c.Swaths();
@@ -28,7 +28,7 @@ def test_fields2cover_obj_complete_turn_path_obj_computeCost():
   length = f2c.OBJ_CompleteTurnPathObj_Dubins(robot);
 
   # Precision is quite low so this is just checking a prediction is done
-  near(length.computeCost(swath1, swath2), math.pi, 0.1);
+  near(length.computeCost(swath1, swath2), math.pi/2, 0.1);
 
 def test_fields2cover_complete_pred_turn_path_obj_paramsCheck():
   robot = f2c.Robot(2.1, 2.5);
