0bd4c91f2bda0f0e753287a46fc09f8a6efc83cb
Gonzalo Mier <gonzalo.miermunoz@wur.nl>
2023-01-23 16:36:09 +0100

    Add new github action to deploy docs when push to main

--- docs/conf.py
+++ docs/conf.py
@@ -85,6 +85,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
 
+html_baseurl = ''
 html_logo = "figures/logo_fields2cover.png"
 html_favicon = "figures/favicon/favicon.ico"
 html_theme_options = {
