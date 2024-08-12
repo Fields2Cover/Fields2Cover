4b971a6621dfc8606b0a1367caa4f3935d6f675f
Tom <tom1.schut@wur.nl>
2024-08-12 13:23:06 +0200

    feat: data for transportlanes, fix: parser requires name

--- src/fields2cover/utils/parser.cpp
+++ src/fields2cover/utils/parser.cpp
@@ -4,151 +4,159 @@
 //                        BSD-3 License
 //=============================================================================
 
-#include <tinyxml2.h>
-#include <boost/optional.hpp>
-#include <nlohmann/json.hpp>
 #include "fields2cover/utils/parser.h"
 #include "fields2cover/utils/transformation.h"
+#include <boost/optional.hpp>
+#include <nlohmann/json.hpp>
+#include <tinyxml2.h>
 
 using json = nlohmann::json;
 
 namespace f2c {
 
-void Parser::importGml(const std::string& file, F2CFields& fields) {
-  fields.emplace_back(importFieldGml(file));
-}
+void Parser::importGml(const std::string& file, F2CFields& fields) { fields.emplace_back(importFieldGml(file)); }
 
-F2CField Parser::importFieldGml(
-    const std::string& file, bool coord_frame_fail_silently) {
-  // Tinyxml2 depends on locale when parsing files. It may expect float numbers
-  // as "1,5" instead of "1.5", which is parsed as "1". The next line solves
-  // the issue.
-  std::locale::global(std::locale::classic());
+F2CField Parser::importFieldGml(const std::string& file, bool coord_frame_fail_silently)
+{
+    // Tinyxml2 depends on locale when parsing files. It may expect float numbers
+    // as "1,5" instead of "1.5", which is parsed as "1". The next line solves
+    // the issue.
+    std::locale::global(std::locale::classic());
 
-  tinyxml2::XMLDocument doc;
-  doc.LoadFile(file.c_str());
-  auto* p_parcel = doc.RootElement();
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(file.c_str());
+    auto* p_parcel = doc.RootElement();
 
-  if (p_parcel == nullptr) {
-    throw std::invalid_argument("File not found");
-  }
-
-  std::string id {""};
-
-  auto e_result = p_parcel->Attribute("id");
-  if (e_result != nullptr) {
-    id = e_result;
-  }
-
-  auto* p_field = p_parcel->FirstChildElement("Field");
-
-  auto* p_polygon = p_field\
-            ->FirstChildElement("geometry")\
-            ->FirstChildElement("gml:Polygon");
-
-  std::string coord_sys = p_polygon->Attribute("srsName");
-
-  std::string p_coords = std::string(
-            p_polygon->FirstChildElement("gml:outerBoundaryIs")\
-            ->FirstChildElement("gml:LinearRing")\
-            ->FirstChildElement("gml:coordinates")\
-            ->GetText());
-  auto findAndReplaceAll =
-      [](std::string& data, std::string toSearch, std::string replaceStr) {
-            size_t pos = data.find(toSearch);
-            while (pos != std::string::npos) {
-                data.replace(pos, toSearch.size(), replaceStr);
-                pos = data.find(toSearch, pos + replaceStr.size());
-            }
-            return;
-      };
-  findAndReplaceAll(p_coords, ",", ";");
-  findAndReplaceAll(p_coords, " ", ", ");
-  findAndReplaceAll(p_coords, ";", " ");
-  p_coords = "POLYGON ((" + p_coords + "))";
-  OGRGeometry* new_geom{};
-  auto spt_ref = Transform::createSptRef(coord_sys, coord_frame_fail_silently);
-  OGRGeometryFactory::createFromWkt(p_coords.c_str(), spt_ref.get(), &new_geom);
-
-  F2CField field(F2CCells(new_geom), id);
-  field.setCRS(coord_sys);
-  OGRGeometryFactory::destroyGeometry(new_geom);
-
-  return field;
-}
-
-
-F2CPoint getPointFromJson(const json& ps) {
-  if (ps.size() == 3) {
-    return F2CPoint(ps[0], ps[1], ps[2]);
-  } else if (ps.size() == 2) {
-    return F2CPoint(ps[0], ps[1]);
-  }
-  return F2CPoint();
-}
-
-F2CCell getCellFromJson(const json& imported_cell) {
-  F2CLinearRing outer_ring;
-  for (auto&& ps : imported_cell["geometry"]["coordinates"][0]) {
-    outer_ring.addPoint(getPointFromJson(ps));
-  }
-  F2CCell cell(outer_ring);
-  for (int i = 1; i < imported_cell["geometry"]["coordinates"].size(); ++i) {
-    F2CLinearRing inner_ring;
-    for (auto&& ps : imported_cell["geometry"]["coordinates"][i]) {
-      inner_ring.addPoint(getPointFromJson(ps));
+    if (p_parcel == nullptr) {
+        throw std::invalid_argument("File not found");
     }
-    cell.addRing(inner_ring);
-  }
-  return cell;
-}
 
+    std::string id { "" };
 
-int Parser::importJson(const std::string& file, F2CFields& fields) {
-  std::ifstream f(file);
-  json imported_field = json::parse(f);
-
-  for (auto&& imported_cell : imported_field["features"]) {
-    fields.emplace_back(
-      F2CField(F2CCells(getCellFromJson(imported_cell)),
-        imported_cell["properties"]["Name"]));
-  }
-  return 0;
-}
-
-F2CCell Parser::importCellJson(const std::string& file) {
-  std::ifstream f(file);
-  json imported_field = json::parse(f);
-  return getCellFromJson(imported_field["features"][0]);
-}
-
-F2CSwaths Parser::importSwathsJson(const std::string& file) {
-  std::ifstream f(file);
-  json imported_swaths = json::parse(f);
-  F2CSwaths swaths;
-  for (auto&& imported_swath : imported_swaths["features"]) {
-    F2CLineString line;
-    for (auto&& ps : imported_swath["geometry"]["coordinates"]) {
-      line.addPoint(getPointFromJson(ps));
+    auto e_result = p_parcel->Attribute("id");
+    if (e_result != nullptr) {
+        id = e_result;
     }
-    swaths.emplace_back(line,
-      imported_swath["properties"]["width"],
-      imported_swath["properties"]["path_id"]);
-  }
-  return swaths;
+
+    auto* p_field = p_parcel->FirstChildElement("Field");
+
+    auto* p_polygon = p_field->FirstChildElement("geometry")->FirstChildElement("gml:Polygon");
+
+    std::string coord_sys = p_polygon->Attribute("srsName");
+
+    std::string p_coords = std::string(
+        p_polygon->FirstChildElement("gml:outerBoundaryIs")->FirstChildElement("gml:LinearRing")->FirstChildElement("gml:coordinates")->GetText());
+    auto findAndReplaceAll = [](std::string& data, std::string toSearch, std::string replaceStr) {
+        size_t pos = data.find(toSearch);
+        while (pos != std::string::npos) {
+            data.replace(pos, toSearch.size(), replaceStr);
+            pos = data.find(toSearch, pos + replaceStr.size());
+        }
+        return;
+    };
+    findAndReplaceAll(p_coords, ",", ";");
+    findAndReplaceAll(p_coords, " ", ", ");
+    findAndReplaceAll(p_coords, ";", " ");
+    p_coords = "POLYGON ((" + p_coords + "))";
+    OGRGeometry* new_geom {};
+    auto spt_ref = Transform::createSptRef(coord_sys, coord_frame_fail_silently);
+    OGRGeometryFactory::createFromWkt(p_coords.c_str(), spt_ref.get(), &new_geom);
+
+    F2CField field(F2CCells(new_geom), id);
+    field.setCRS(coord_sys);
+    OGRGeometryFactory::destroyGeometry(new_geom);
+
+    return field;
 }
 
-F2CStrips Parser::importStripsJson(const std::string& file) {
-  std::ifstream f(file);
-  json imported_strips = json::parse(f);
-  F2CStrips strips;
-  for (auto&& imported_strip : imported_strips["features"]) {
-    F2CStrip strip;
-    strip.setName(imported_strip["properties"]["crop_id"]);
-    strip.setCell(getCellFromJson(imported_strip));
-    strips.emplace_back(strip);
-  }
-  return strips;
+F2CPoint getPointFromJson(const json& ps)
+{
+    if (ps.size() == 3) {
+        return F2CPoint(ps[0], ps[1], ps[2]);
+    } else if (ps.size() == 2) {
+        return F2CPoint(ps[0], ps[1]);
+    }
+    return F2CPoint();
 }
 
-}  // namespace f2c
+F2CCell getCellFromJson(const json& imported_cell)
+{
+    F2CLinearRing outer_ring;
+    for (auto&& ps : imported_cell["geometry"]["coordinates"][0]) {
+        outer_ring.addPoint(getPointFromJson(ps));
+    }
+    F2CCell cell(outer_ring);
+    for (int i = 1; i < imported_cell["geometry"]["coordinates"].size(); ++i) {
+        F2CLinearRing inner_ring;
+        for (auto&& ps : imported_cell["geometry"]["coordinates"][i]) {
+            inner_ring.addPoint(getPointFromJson(ps));
+        }
+        cell.addRing(inner_ring);
+    }
+    return cell;
+}
+
+std::string gen_random(const int len)
+{
+    static const char alphanum[] = "0123456789"
+                                   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                   "abcdefghijklmnopqrstuvwxyz";
+    std::string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+
+    return tmp_s;
+}
+
+int Parser::importJson(const std::string& file, F2CFields& fields)
+{
+    std::ifstream f(file);
+    json imported_field = json::parse(f);
+
+    auto name = imported_field["properties"].contains("Name") ? imported_field["properties"]["Name"].get<std::string>() : "cell " + gen_random(3);
+    for (auto&& imported_cell : imported_field["features"]) {
+        fields.emplace_back(F2CField(F2CCells(getCellFromJson(imported_cell)), name));
+    }
+    return 0;
+}
+
+F2CCell Parser::importCellJson(const std::string& file)
+{
+    std::ifstream f(file);
+    json imported_field = json::parse(f);
+    return getCellFromJson(imported_field["features"][0]);
+}
+
+F2CSwaths Parser::importSwathsJson(const std::string& file)
+{
+    std::ifstream f(file);
+    json imported_swaths = json::parse(f);
+    F2CSwaths swaths;
+    for (auto&& imported_swath : imported_swaths["features"]) {
+        F2CLineString line;
+        for (auto&& ps : imported_swath["geometry"]["coordinates"]) {
+            line.addPoint(getPointFromJson(ps));
+        }
+        swaths.emplace_back(line, imported_swath["properties"]["width"], imported_swath["properties"]["path_id"]);
+    }
+    return swaths;
+}
+
+F2CStrips Parser::importStripsJson(const std::string& file)
+{
+    std::ifstream f(file);
+    json imported_strips = json::parse(f);
+    F2CStrips strips;
+    for (auto&& imported_strip : imported_strips["features"]) {
+        F2CStrip strip;
+        strip.setName(imported_strip["properties"]["crop_id"]);
+        strip.setCell(getCellFromJson(imported_strip));
+        strips.emplace_back(strip);
+    }
+    return strips;
+}
+
+} // namespace f2c
--- tutorials/8_complete_flow.cpp
+++ tutorials/8_complete_flow.cpp
@@ -4,47 +4,77 @@
 //                        BSD-3 License
 //=============================================================================
 
-
 #include "fields2cover.h"
 #include <iostream>
 
-int main() {
-  // Import field
-  F2CField field = f2c::Parser::importFieldGml(std::string(DATA_PATH) + "test1.xml");
-  F2CField orig_field = field.clone();
-  // Transform into UTM to work in meters
-  f2c::Transform::transformToUTM(field);
+int main()
+{
+    // Import field
+    // F2CField field = f2c::Parser::importFieldGml(std::string(DATA_PATH) + "test1.xml");
+    auto field_json = f2c::Parser::importCellJson(std::string(DATA_PATH) + "field.geojson");
+    auto field_cells = F2CCells(field_json);
+    auto field = F2CField(field_cells);
+    F2CSwaths imported_swaths = f2c::Parser::importSwathsJson(std::string(DATA_PATH) + "workinglanes.geojson");
 
-  F2CRobot robot (2.0, 6.0);
-  f2c::hg::ConstHL const_hl;
-  F2CCells no_hl = const_hl.generateHeadlands(field.getField(), 3.0 * robot.getWidth());
-  f2c::sg::BruteForce bf;
-  F2CSwaths swaths = bf.generateSwaths(M_PI, robot.getCovWidth(), no_hl.getGeometry(0));
-  f2c::rp::SnakeOrder snake_sorter;
-  swaths = snake_sorter.genSortedSwaths(swaths);
-  f2c::pp::PathPlanning path_planner;
-  robot.setMinTurningRadius(2);  // m
-  f2c::pp::DubinsCurves dubins;
-  F2CPath path = path_planner.planPath(robot, swaths, dubins);
+    F2CFields fields;
+    f2c::Parser::importJson(std::string(DATA_PATH) + "transportlanes.geojson", fields);
+    // convert fields to cell array
+    F2CCells transport_lanes_cells;
+    for (auto& field : fields) {
+        transport_lanes_cells.addGeometry(field.getCellsAbsPosition().getGeometry(0));
+    }
 
+    // F2CField orig_field = field.clone();
+    // Transform into UTM to work in meters
+    // f2c::Transform::transformToUTM(field);
 
-  f2c::Visualizer::figure();
-  f2c::Visualizer::plot(field);
-  f2c::Visualizer::plot(no_hl);
-  f2c::Visualizer::plot(path);
-  f2c::Visualizer::save("Tutorial_8_1_UTM.png");
+    F2CRobot robot(2.0, 6.0);
+    f2c::hg::ConstHL const_hl;
+    // F2CCells path_middle_hl = const_hl.generateHeadlands(field_cells, 3.0 * robot.getWidth());
+    f2c::sg::BruteForce bf;
+    // F2CSwaths swaths = bf.generateSwaths(M_PI, robot.getCovWidth(), no_hl.getGeometry(0));
 
+    // this is the route planner, it sorts the swaths, that is all.
+    f2c::rp::SnakeOrder snake_sorter;
+    auto swaths = snake_sorter.genSortedSwaths(imported_swaths);
 
-  // Transform the generated path back to the previousa CRS.
-  F2CPath path_gps = f2c::Transform::transformToPrevCRS(path, field);
-  f2c::Transform::transformToPrevCRS(field);
+    f2c::rp::BoustrophedonOrder boustrophedon_sorter;
+    // auto swaths = boustrophedon_sorter.genSortedSwaths(imported_swaths);
 
-  f2c::Visualizer::figure();
-  f2c::Visualizer::plot(orig_field.getCellsAbsPosition());
-  f2c::Visualizer::plot(path_gps);
-  f2c::Visualizer::save("Tutorial_8_1_GPS.png");
+    f2c::rp::RoutePlannerBase route_planner;
+    auto swathsByCells = new f2c::types::SwathsByCells();
+    swathsByCells->emplace_back(swaths);
 
-  return 0;
+    F2CRoute route = route_planner.genRoute(transport_lanes_cells, *swathsByCells);
+
+    // the path planner actually creates a path from the sorted swaths.
+    f2c::pp::PathPlanning path_planner;
+    f2c::pp::DubinsCurves dubins;
+    robot.setMinTurningRadius(2); // m
+    F2CPath path = path_planner.planPath(robot, route, dubins);
+
+    f2c::Visualizer::figure();
+    // f2c::Visualizer::plot(field_cells);
+    // f2c::Visualizer::plot(transport_lanes_cells);
+    // f2c::Visualizer::plot(swaths);
+
+    f2c::Visualizer::plot(route);
+    // f2c::Visualizer::show();
+    f2c::Visualizer::plot(path);
+    f2c::Visualizer::show();
+    // f2c::Visualizer::save("Tutorial_8_1_UTM.png");
+
+    // f2c::Visualizer::plot(route);
+    // f2c::Visualizer::plot(path);
+    // f2c::Visualizer::show();
+    // Transform the generated path back to the previousa CRS.
+    // F2CPath path_gps = f2c::Transform::transformToPrevCRS(path, field);
+    // f2c::Transform::transformToPrevCRS(field);
+
+    // f2c::Visualizer::figure();
+    // f2c::Visualizer::plot(field.getCellsAbsPosition());
+    // f2c::Visualizer::plot(path_gps);
+    // f2c::Visualizer::save("Tutorial_8_1_GPS.png");
+
+    return 0;
 }
-
-
