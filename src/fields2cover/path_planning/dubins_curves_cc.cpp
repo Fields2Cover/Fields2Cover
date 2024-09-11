287ccae085749541f4f2bf886abcbf5964eda07a
vinny <vinny.ruia@fireflyautomatix.com>
2024-09-11 08:20:29 -0400

    optionally turn off loop detection, default false

--- src/fields2cover/path_planning/dubins_curves_cc.cpp
+++ src/fields2cover/path_planning/dubins_curves_cc.cpp
@@ -9,69 +9,80 @@
 #include "fields2cover/path_planning/steer_to_path.hpp"
 #include "fields2cover/path_planning/dubins_curves_cc.h"
 
-namespace f2c::pp {
+namespace f2c::pp
+{
 
-F2CPath DubinsCurvesCC::createSimpleTurn(const F2CRobot& robot,
-    double dist_start_pos, double start_angle, double end_angle) {
-  steer::State start, end;
+  F2CPath DubinsCurvesCC::createSimpleTurn(const F2CRobot &robot,
+                                           double dist_start_pos, double start_angle, double end_angle)
+  {
+    steer::State start, end;
 
-  start.x = 0.0;
-  start.y = 0.0;
-  start.theta = start_angle;
-  start.kappa = 0.0;
-  start.d = 0;
+    start.x = 0.0;
+    start.y = 0.0;
+    start.theta = start_angle;
+    start.kappa = 0.0;
+    start.d = 0;
 
-  end.x = dist_start_pos;
-  end.y = 0.0;
-  end.theta = end_angle;
-  end.kappa = 0.0;
-  end.d = 0;
+    end.x = dist_start_pos;
+    end.y = 0.0;
+    end.theta = end_angle;
+    end.kappa = 0.0;
+    end.d = 0;
 
-  double n = 0.0;
-  while (true) {
-    CC00_Dubins_State_Space ss(
-        robot.getMaxCurv() / (1+0.05*n),
-        robot.getMaxDiffCurv() / (1+0.2*n),
-        discretization,
-        true);
-    if (loop_detected(ss.get_controls(start, end)) && n <= 20.) {
-      n += 1.0;
-    } else {
-      return steerStatesToPath(ss.get_path(start, end),
-          robot.getTurnVel());
+    double n = 0.0;
+    while (true)
+    {
+      CC00_Dubins_State_Space ss(
+          robot.getMaxCurv() / (1 + 0.05 * n),
+          robot.getMaxDiffCurv() / (1 + 0.2 * n),
+          discretization,
+          true);
+      if (loop_detected(ss.get_controls(start, end)) && n <= 20.)
+      {
+        n += 1.0;
+      }
+      else
+      {
+        return steerStatesToPath(ss.get_path(start, end),
+                                 robot.getTurnVel());
+      }
     }
   }
-}
 
-F2CPath DubinsCurvesCC::createConstrainedTurn(const F2CRobot& robot,
-    const F2CPoint& start_pos, double start_angle, double start_curvature,
-    const F2CPoint& end_pos, double end_angle, double end_curvature) {
-  steer::State start, end;
-  start.x = start_pos.getX();
-  start.y = start_pos.getY();
-  start.theta = start_angle;
-  start.kappa = start_curvature;
-  start.d = 0;
+  F2CPath DubinsCurvesCC::createConstrainedTurn(const F2CRobot &robot,
+                                                const F2CPoint &start_pos, double start_angle, double start_curvature,
+                                                const F2CPoint &end_pos, double end_angle, double end_curvature, const bool fix_loops)
+  {
+    steer::State start, end;
+    start.x = start_pos.getX();
+    start.y = start_pos.getY();
+    start.theta = start_angle;
+    start.kappa = start_curvature;
+    start.d = 0;
 
-  end.x = end_pos.getX();
-  end.y = end_pos.getY();
-  end.theta = end_angle;
-  end.kappa = end_curvature;
-  end.d = 0;
-  double n = 0.0;
-  while (true) {
-    CCpmpm_Dubins_State_Space ss(
-        robot.getMaxCurv() / (1+0.05*n),
-        robot.getMaxDiffCurv() / (1+0.2*n),
-        discretization,
-        true);
-    if (loop_detected(ss.get_controls(start, end)) && n <= 20.) {
-      n += 1.0;
-    } else {
-      return steerStatesToPath(ss.get_path(start, end),
-          robot.getTurnVel());
+    end.x = end_pos.getX();
+    end.y = end_pos.getY();
+    end.theta = end_angle;
+    end.kappa = end_curvature;
+    end.d = 0;
+    double n = 0.0;
+
+    while (true)
+    {
+      CCpmpm_Dubins_State_Space ss(
+          robot.getMaxCurv() / (1 + 0.05 * n),
+          robot.getMaxDiffCurv() / (1 + 0.2 * n),
+          discretization,
+          true);
+      if (fix_loops && loop_detected(ss.get_controls(start, end)) && n <= 20.)
+      {
+        n += 1.0;
+      }
+      else
+      {
+        return steerStatesToPath(ss.get_path(start, end),
+                                 robot.getTurnVel());
+      }
     }
   }
-}
-}  // namespace f2c::pp
-
+} // namespace f2c::pp
